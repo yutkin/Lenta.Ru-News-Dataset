505556290bc296eac78017dd01d40558b521f73a
anna244 <uncle-bensico@ya.ru>
2022-11-17 19:09:29 +0500

    Update parser for new layout on lenta.ru website. Add ability to extend existing csv file with new parsed data.

--- download_lenta.py
+++ download_lenta.py
@@ -2,9 +2,13 @@ import argparse
 import asyncio
 import csv
 import logging
+import os
+import typing
 from concurrent.futures import ProcessPoolExecutor
 from datetime import datetime, timedelta
+from itertools import chain
 from multiprocessing import cpu_count
+from urllib.parse import urljoin
 
 import aiohttp
 from bs4 import BeautifulSoup
@@ -23,7 +27,7 @@ class LentaParser:
     default_parser = "html.parser"
 
     def __init__(self, *, max_workers: int, outfile_name: str, from_date: str):
-        self._endpoint = "https://lenta.ru/news"
+        self._endpoint = "https://lenta.ru/"
 
         self._sess = None
         self._connector = None
@@ -31,29 +35,47 @@ class LentaParser:
         self._executor = ProcessPoolExecutor(max_workers=max_workers)
 
         self._outfile_name = outfile_name
+        self._is_outfile_exists = os.path.exists(self._outfile_name)
         self._outfile = None
         self._csv_writer = None
         self.timeouts = aiohttp.ClientTimeout(total=60, connect=60)
 
-        self._n_downloaded = 0
+        self._existing_urls = set()
+        if self._is_outfile_exists:
+            with open(self._outfile_name, "r", encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    self._existing_urls.add(row["url"])
+
+        self._n_downloaded = len(self._existing_urls)
         self._from_date = datetime.strptime(from_date, "%d.%m.%Y")
+
+        self._ignored_news_prefixes = (
+            "https://lenta.ru/extlink/",
+            "/extlink/",
+            "/sport/",
+            "/themes/",
+            "/tags/",
+        )
 
     @property
     def dates_countdown(self):
         date_start, date_end = self._from_date, datetime.today()
 
         while date_start <= date_end:
-            yield date_start.strftime("%Y/%m/%d")
+            yield date_start
             date_start += timedelta(days=1)
 
     @property
     def writer(self):
         if self._csv_writer is None:
-            self._outfile = open(self._outfile_name, "w", 1)
+            mode = "a" if self._is_outfile_exists else "w"
+            self._outfile = open(self._outfile_name, mode=mode, buffering=1, encoding="utf-8")
             self._csv_writer = csv.DictWriter(
-                self._outfile, fieldnames=["url", "title", "text", "topic", "tags"]
+                self._outfile, fieldnames=["date", "url", "topic", "tags", "title", "text"]
             )
-            self._csv_writer.writeheader()
+            if not self._is_outfile_exists:
+                self._csv_writer.writeheader()
 
         return self._csv_writer
 
@@ -78,35 +100,51 @@ class LentaParser:
     @staticmethod
     def parse_article_html(html: str):
         doc_tree = BeautifulSoup(html, LentaParser.default_parser)
-        tags = doc_tree.find("a", "item dark active")
+        tags = doc_tree.find("a", "rubric-header__link _active")
         tags = tags.get_text() if tags else None
 
-        body = doc_tree.find("div", attrs={"itemprop": "articleBody"})
+        body = doc_tree.find("div", "topic-body__content")
 
         if not body:
-            raise RuntimeError(f"Article body is not found")
+            raise RuntimeError("Article body is not found")
 
-        text = " ".join([p.get_text() for p in body.find_all("p")])
+        text = " ".join([p.get_text() for p in body.find_all("p", "topic-body__content-text")])
 
-        topic = doc_tree.find("a", "b-header-inner__block")
+        topic = doc_tree.find("a", "topic-header__item topic-header__rubric")
         topic = topic.get_text() if topic else None
 
-        title = doc_tree.find("h1", attrs={"itemprop": "headline"})
+        title = doc_tree.find("h1", "topic-body__titles")
         title = title.get_text() if title else None
 
         return {"title": title, "text": text, "topic": topic, "tags": tags}
 
-    @staticmethod
-    def _extract_urls_from_html(html: str):
+    def _fetch_next_page_url(self, html: str) -> typing.Optional[str]:
         doc_tree = BeautifulSoup(html, LentaParser.default_parser)
-        news_list = doc_tree.find_all("div", "item news b-tabloid__topic_news")
-        return tuple(f"https://lenta.ru{news.find('a')['href']}" for news in news_list)
+        nex_url_buttons = doc_tree.find_all("a", "loadmore js-loadmore _two-buttons")
+        for item in nex_url_buttons:
+            if item.find(string="Дальше"):
+                return urljoin(self._endpoint, item.attrs["href"])
 
-    async def _fetch_all_news_on_page(self, html: str):
+    @staticmethod
+    def _extract_urls_from_html(endpoint: str, ignored_news_prefixes: typing.Tuple[str], html: str):
+        doc_tree = BeautifulSoup(html, LentaParser.default_parser)
+        news_list_top = doc_tree.find_all("a", "card-full-other _archive")
+        news_list_bottom = doc_tree.find_all("a", "card-full-news _archive")
+        return tuple(
+            news["href"] if news["href"].startswith("http") else urljoin(endpoint, news["href"])
+            for news in chain(news_list_top, news_list_bottom)
+            if not news["href"].startswith(ignored_news_prefixes)
+        )
+
+    async def _fetch_all_news_on_page(self, date: datetime, html: str):
         # Get news URLs from raw html
         loop = asyncio.get_running_loop()
         news_urls = await loop.run_in_executor(
-            self._executor, self._extract_urls_from_html, html
+            self._executor, 
+            self._extract_urls_from_html, 
+            self._endpoint, 
+            self._ignored_news_prefixes, 
+            html
         )
 
         # Fetching news
@@ -137,8 +175,10 @@ class LentaParser:
             except Exception:
                 logger.exception(f"Cannot parse {url}")
             else:
-                parse_res["url"] = url
-                parsed_news.append(parse_res)
+                if url not in self._existing_urls:
+                    parse_res["url"] = url
+                    parse_res["date"] = date.strftime("%Y/%m/%d")
+                    parsed_news.append(parse_res)
 
         if parsed_news:
             self.writer.writerows(parsed_news)
@@ -161,24 +201,27 @@ class LentaParser:
 
     async def _producer(self):
         for date in self.dates_countdown:
-            news_page_url = f"{self._endpoint}/{date}"
+            news_page_url = urljoin(self._endpoint, date.strftime("%Y/%m/%d"))
 
-            try:
-                html = await asyncio.create_task(self.fetch(news_page_url))
-            except aiohttp.ClientResponseError:
-                logger.exception(f"Cannot fetch {news_page_url}")
-            except aiohttp.ClientConnectionError:
-                logger.exception(f"Cannot fetch {news_page_url}")
-            else:
-                n_proccessed_news = await self._fetch_all_news_on_page(html)
+            while news_page_url:
+                try:
+                    html = await asyncio.create_task(self.fetch(news_page_url))
+                except aiohttp.ClientResponseError:
+                    logger.exception(f"Cannot fetch {news_page_url}")
+                except aiohttp.ClientConnectionError:
+                    logger.exception(f"Cannot fetch {news_page_url}")
+                else:
+                    n_proccessed_news = await self._fetch_all_news_on_page(date, html)
 
-                if n_proccessed_news == 0:
-                    logger.info(f"News not found at {news_page_url}.")
+                    if n_proccessed_news == 0:
+                        logger.info(f"News not found at {news_page_url}.")
 
-                logger.info(
-                    f"{news_page_url} processed ({n_proccessed_news} news). "
-                    f"{self._n_downloaded} news saved totally."
-                )
+                    logger.info(
+                        f"{news_page_url} processed ({n_proccessed_news} news). "
+                        f"{self._n_downloaded} news saved totally."
+                    )
+
+                    news_page_url = self._fetch_next_page_url(html)
 
     async def run(self):
         try:
